08b050d1cf1b72586ad5de6b982c63b505c32e93
Tejas Wakchaure <wakchauretejas2@gmail.com>
2023-06-16 21:02:54 +0530

    use Route Link Tag

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>This is for practice </h1>
+      <BrowserRouter>
+        <MyNavigationLink />
+        <Routes>
+          <Route path="/home" element={<h1>Home</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
+function MyNavigationLink() {
+  return (
+    <>
+      <Link to={"/home"}>Home |</Link>
+      <Link to={"/registration"}> Registration |</Link>
+    </>
+  );
+}
 export default App;
